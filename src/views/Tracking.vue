c7e2c98784c75d46f79df5e3c00208086a93953f
Achilles45 <achillesusuoyibo7@gmail.com>
2020-05-22 11:52:33 -0700

    Changed map image

--- src/views/Tracking.vue
+++ src/views/Tracking.vue
@@ -143,13 +143,13 @@ export default {
 
 <style lang="scss" scoped>
 .track {
-  background: linear-gradient(rgba(4, 9, 30, 0.8), rgba(4, 9, 30, 0.8)),
+  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
     url("../assets/images/map.jpg");
   background-size: cover;
   background-position: center;
   background-attachment: fixed;
   .track__heading {
-    background: linear-gradient(rgba(4, 9, 30, 0.8), rgba(4, 9, 30, 0.9)),
+    background: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)),
       url("../assets/images/map.jpg");
     background-size: cover;
     background-position: center;
