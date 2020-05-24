4edbf6b7e06d1c59037809dce21add658ff7d4b2
Achilles45 <achillesusuoyibo7@gmail.com>
2020-05-23 20:31:03 -0700

    Changed firebase

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 var firebaseConfig = {
-  apiKey: "AIzaSyCliJoFZ9BCAOjlBM20KUbSvvJpcNsAabk",
-  authDomain: "logisticsapp-5104a.firebaseapp.com",
-  databaseURL: "https://logisticsapp-5104a.firebaseio.com",
-  projectId: "logisticsapp-5104a",
-  storageBucket: "logisticsapp-5104a.appspot.com",
-  messagingSenderId: "983869454864",
-  appId: "1:983869454864:web:ccdc283efdd2b47f20a895",
-  measurementId: "G-Y1FRFNYY2N"
+  apiKey: "AIzaSyCfuK1A1HsRnY-P_HgQLLdyaDZjSfPBX9E",
+  authDomain: "logistics-app-68b9b.firebaseapp.com",
+  databaseURL: "https://logistics-app-68b9b.firebaseio.com",
+  projectId: "logistics-app-68b9b",
+  storageBucket: "logistics-app-68b9b.appspot.com",
+  messagingSenderId: "467381804168",
+  appId: "1:467381804168:web:d60784bce8dfae7f49d33d",
+  measurementId: "G-HLKJC66GHR"
 };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
